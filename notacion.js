66bc7e1d33dcfeff0f31e3c7541c3929d6acd429
juliandmejia <juli08dmejia@hotmail.com>
2023-05-08 16:19:34 -0500

    Nuevo archivo notacion

---
+++ notacion.js
@@ -0,0 +1,30 @@
+//Notación 
+// ; --> Delimitar el final de una linea
+const b = 4;
+
+b + 4 
+
+// .  --> Se utiliza en los objetos para acceder a los atributos
+// movil.anchura
+
+// [] --> listas, arreglos arrays
+const ar = [1,2,3,4] 
+
+console.log(ar)
+
+// () --> Funciones
+function suma(a, b){
+    //se escribe la funcion
+}
+
+// {} --> Llaves para objetos, funciones y estucturas de control
+const movil = {
+    anchura: 5 ,
+    altura: 10
+}
+
+if (true){
+    //todo lo que haya entre llaves
+
+    const constante2 = "Hola"
+}
