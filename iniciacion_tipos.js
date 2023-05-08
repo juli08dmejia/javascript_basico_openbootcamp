162bad75c4e541ec2b210ceb8fb2f07025b5e021
juliandmejia <juli08dmejia@hotmail.com>
2023-05-08 16:06:19 -0500

    NUevo archivo agregado

---
+++ iniciacion_tipos.js
@@ -0,0 +1,27 @@
+//Tipos primitivos
+
+//Number
+4;
+
+//String
+"Hola mundo";
+'Hola mundo';
+
+//Booleanos
+true;
+false;
+
+//nulo  y undefine
+null;
+undefined; undefined
+
+
+
+// null, undefined, false, 0  ==>
+console.log(null == undefined) 
+
+if (true){
+    console.log("cumple")
+}else{
+    console.log("no cumple")
+}
