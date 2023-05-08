da51ca9d25827948d598ab38e33e55423ca2eb06
juli08dmejia <juli08dmejia@hotmail.com>
2023-05-08 12:08:20 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//Este es el mensaje de Bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+
+/*
+Esta es la parte intermedia del proyecto
+En esta parte realizamos los chequeos necesarios
+Por ultimo vamos a la ultima linea de salida
+Esto también está comentado
+*/
+
+
+
+//Este es el mensaje que se usa para despedir
+console.log("Adiós")
