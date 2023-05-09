560f0cd222d94984aba73aa50a7152f0f735731d
juliandmejia <juli08dmejia@hotmail.com>
2023-05-09 15:36:57 -0500

    Se agrego archivo datos

---
+++ datos.js
@@ -0,0 +1,11 @@
+const datos_pers = ['Julian David', 32, true, 19900731]
+
+console.log(datos_pers)
+
+datos_pers.libro = {
+    titulo: 'El poder de la disciplina',
+    autor: 'Raimon Samsó',
+    fecha: '2019',
+    url:'https://pdfcoffee.com/disciplina-7-pdf-free.html',
+}
+console.log(datos_pers)
