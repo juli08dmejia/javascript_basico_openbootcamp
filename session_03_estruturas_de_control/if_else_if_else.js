a0d3009152d4e82f71cdf1752b805701ce1a959b
juliandmejia <juli08dmejia@hotmail.com>
2023-05-09 17:41:11 -0500

    Trabajando en comparacion

---
+++ session_03_estruturas_de_control/if_else_if_else.js
@@ -0,0 +1,22 @@
+//if else + if else
+
+let nota = 5 ;
+
+if (nota === 5 ){
+    console.log("Has obtenido un Sobresaliente")
+}
+else if (nota  === 4 ) {
+    console.log("Buen trabajo pero podrías haberlo hecho mucho mejor")
+}
+else if (nota  === 3 ) {
+    console.log("Has obtenido un suficiente")
+}
+else if (nota  === 2 ) {
+    console.log("No has aprobado por poco")
+}
+else if (nota  === 1 ) {
+    console.log("No has estudiado nada, trabaja un poquito mas para la proxima")
+}
+else {
+    console.log("Error, introduce una nota entre el 1 y el 5")
+}
---
+++ session_03_estruturas_de_control/switch.js
@@ -0,0 +1,26 @@
+//Sentencias Switch
+
+let nota = 5;
+
+switch (nota){
+    case 5:
+        console.log("Buen trabajo, sobresaliente") ;
+        break;
+    case 4:
+        console.log("Buen trabajo, pero podrás haberlo hecho mejor")  ;
+        break;
+    case 3:
+        console.log("Has obtenido un suficiente") ;  
+        break;
+    case 2:
+        console.log("No has aprobado por poco") ;
+        break;
+    case 1:
+        console.log("No has hecho nada, trabaja un poquito más para la proxima")  ;
+        break;
+    default:
+        console.log("Error")  ;
+        break;
+
+}
+
