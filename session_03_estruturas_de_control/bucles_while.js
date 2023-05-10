ac6494385b9304d1a50b81076d1724b1f858b20c
juliandmejia <juli08dmejia@hotmail.com>
2023-05-10 11:38:01 -0500

    Trabando en bucles for - while

---
+++ session_03_estruturas_de_control/bucles_while.js
@@ -0,0 +1,32 @@
+//Bucles While
+
+//   condicion 
+
+let i = 0 ;
+let max = 10 ;
+
+
+while (i < max){
+    console.log(i) ;
+    i++ ;
+}
+
+// Do..while
+i = 15 ;
+
+do {
+    console.log("EStoy en el Do WHile") ; 
+}while (i < max){
+    console.log(i) ;
+}
+
+
+
+
+
+
+
+
+
+
+
