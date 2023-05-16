6760e567922f96f7910059bd1a5fa7e6819adc61
juliandmejia <juli08dmejia@hotmail.com>
2023-05-16 16:23:20 -0500

    Trabando con casteo y extremos ejercicios

---
+++ session_05_numbers/ejercicios.js
@@ -0,0 +1,28 @@
+//Una variable que contenga tu altura en centímetros (entero)
+let mi_altura = parseInt(168) ;
+console.log(mi_altura);
+
+//Una variable que contenga tu altura en metros (número de coma flotante)
+let cmi_altura = parseFloat(1.68) ;
+console.log(cmi_altura);
+
+//Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let mi_peso = parseFloat(62)
+console.log(mi_peso)
+
+//Una variable que contenga tu altura en metros redondeada hacia arriba
+console.log(cmi_altura.toFixed(1))
+
+//Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+console.log(cmi_altura.toFixed(3))
+
+//Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let min_valor_JS = Number.MIN_VALUE + 1;
+let max_valor_JS = Number.MAX_VALUE + 1;
+
+console.log(max_valor_JS)
+
+
+
+
+
