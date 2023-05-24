cce9d975ef5d018a402d91b66750508d0be16c4c
juliandmejia <juli08dmejia@hotmail.com>
2023-05-24 17:20:57 -0500

    Funciones carga y sobrecarga

---
+++ session_08_funciones/carga_y_sobrecarga_de_funciones.js
@@ -0,0 +1,25 @@
+//Carga y sobrecarga de funciones
+
+function saludar(){
+    hola()
+}
+
+function hola(){
+    console.log("Hola! soy la funcion hola()")
+}
+
+saludar()
+
+//1. global()
+//2. saludar() glogal() 
+//3. hola() saludar() global()
+//4. saludar() global()
+//5. global()
+
+//function recursivo(){
+//    recursivo()    
+//}
+//recursivo()
+
+
+
