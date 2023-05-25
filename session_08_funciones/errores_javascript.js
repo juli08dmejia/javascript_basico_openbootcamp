cc51fe7b0464a6879d727e590a7d917f3aa3feab
juliandmejia <juli08dmejia@hotmail.com>
2023-05-25 16:27:56 -0500

    manejo de errores

---
+++ session_08_funciones/errores_javascript.js
@@ -0,0 +1,26 @@
+const miFuncion = val => {
+    if (typeof val === "number"){
+        return 2* val 
+    }
+    //return false ;
+    throw new Error("El valor debe ser de tipo número")  
+}
+
+//const elDoble = miFuncion("ala")
+const numero = "8" ;
+
+try{
+    //Código puede fallar
+    console.log("Esta ejecutándose de manera correcta")
+    const doble = miFuncion(numero)
+    console.log(doble)
+}catch(e){
+    //En caso de fallar, quiero que ejecutes
+    console.error(`El valor de e es: ${e}`)
+    console.error(e)
+}finally{
+    console.log("Esto se va a ejecutar tanto si se produce algún error, como si no existe ninguno")
+}
+
+//INternalError , SyntaxError, TypeError, RangeError y ReferenceError
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Error
