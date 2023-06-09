539303153d9e2c9687275fc401f390e778c84390
juliandmejia <juli08dmejia@hotmail.com>
2023-06-09 11:00:52 -0500

    trabajando con eslint

---
+++ session_13_eslint/eslint.npm/index.js
@@ -0,0 +1,27 @@
+//Este es el archivo que vamos a utilizar
+
+const nombre = "Julian"
+
+
+/* eslint-disable */
+
+const persona2 = 'Maria'
+/* eslint-enable */
+
+//En esta línea quiero tener comillas simples (quiero que me desactive la regla de las comillas dobles)
+const nuevoString = 'Esto es un nuevo String'; //eslint-disable-line 
+
+/* eslint-disable-next-line indent */
+    const string2 = "Más strings"
+
+console.log("hola")
+
+const persona3 = "Maria"
+
+const nombre3 = "JUlian" 
+
+console.log(4)
+
+
+
+
